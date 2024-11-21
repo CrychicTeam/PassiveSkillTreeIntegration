93960ba2ea639601fe0aac28d831c5e4827ef0bd
M1hono <xche0557@uni.sydney.edu.au>
2024-11-22 00:11:43 +1100

    - Add EnchantedCondition Compat for

---
+++ src/main/java/org/crychicteam/passiveintegration/mixins/EnchantedConditionMixin.java
@@ -0,0 +1,18 @@
+package org.crychicteam.passiveintegration.mixins;
+
+import com.mrcrayfish.guns.item.GunItem;
+import daripher.skilltree.skill.bonus.condition.item.EnchantedCondition;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(EnchantedCondition.class)
+public class EnchantedConditionMixin {
+    @Inject(method = "met", at = @At("RETURN"), remap = false, cancellable = true)
+    private void passiveIntegration$met(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(cir.getReturnValue() || ( stack.getItem() instanceof GunItem && !EnchantmentHelper.getEnchantments(stack).isEmpty() ));
+    }
+}
