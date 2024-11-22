e7e5714587f7ed99ffaf87f4d9a0c08737491178
Guda chen <xche0557@uni.sydney.edu.au>
2024-11-22 20:38:44 +1100

    - Make Combat with GunModifiers' Critical modifiers. - v.0.0.7

--- src/main/java/org/crychicteam/passiveintegration/mixins/cgm/bonuscompat/crithandler/ProjectileEntityCritMixin.java
+++ src/main/java/org/crychicteam/passiveintegration/mixins/cgm/bonuscompat/crithandler/ProjectileEntityCritMixin.java
@@ -2,14 +2,14 @@ package org.crychicteam.passiveintegration.mixins.cgm.bonuscompat.crithandler;
 
 import com.mrcrayfish.guns.entity.ProjectileEntity;
 import com.mrcrayfish.guns.init.ModDamageTypes;
+import com.mrcrayfish.guns.util.GunModifierHelper;
 import daripher.skilltree.mixin.AbstractArrowAccessor;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.CriticalHitEvent;
 import org.crychicteam.passiveintegration.config.CgmConfig;
@@ -19,7 +19,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
@@ -46,9 +45,12 @@ public abstract class ProjectileEntityCritMixin implements AbstractArrowAccessor
             );
 
             float criticalChance = BonusHandler.getCritChance(player, source, null);
+
+            criticalChance = Mth.clamp(criticalChance + GunModifierHelper.getCriticalChance(weapon), 0.0F, 1.0F);
+
             if(rand.nextFloat() < criticalChance) {
                 float criticalMultiplier = 1.5F + BonusHandler.getCritDamageMultiplier(player, source, null);
-                var criticalEvent = new CriticalHitEvent(player, (ProjectileEntity)(Object)this, damage, true);
+                var criticalEvent = new CriticalHitEvent(player, (ProjectileEntity)(Object)this, damage * criticalMultiplier, true);
                 MinecraftForge.EVENT_BUS.post(criticalEvent);
                 cir.setReturnValue(damage * criticalMultiplier);
             } else {
